381858c687aa70870b19c6dc6b3115a711d3e632
robertovicario <robertovicario.com@gmail.com>
2025-05-21 17:28:35 +0200

    Deployed the app

--- cmd.sh
+++ cmd.sh
@@ -49,7 +49,7 @@ build() {
     rm -f build/app/README.md
     cp .gitattributes build
     cp .gitignore build
-    cp ./app/Dockerfile build
+    cp Dockerfile build
     cp app/README.md build
 
     # -------------------------
@@ -72,7 +72,7 @@ deploy() {
     cp -r app 2UML/app
     cp .gitattributes 2UML
     cp .gitignore 2UML
-    cp ./app/Dockerfile 2UML
+    cp Dockerfile 2UML
     cp app/README.md 2UML
 
     # -------------------------
